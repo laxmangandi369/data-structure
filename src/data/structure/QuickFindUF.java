8a6e3fcde7a6d64858fd02cf0d3547053e0af861
tanya66tanya <rrohan419@gmail.com>
2023-04-21 10:14:03 +0530

    quick find impl

--- src/data/structure/QuickFindUF.java
+++ src/data/structure/QuickFindUF.java
@@ -4,6 +4,7 @@ public class QuickFindUF {
 
 	private int[] id;
 
+	//constructor for initialization the int array with the same value as their index
 	public QuickFindUF(int n) {
 		id = new int[n];
 		for (int i = 0; i < n; i++) {
@@ -11,10 +12,12 @@ public class QuickFindUF {
 		}
 	}
 
+	//checking if the two objects are connect or not will return true or false
 	public boolean connected(int p, int q) {
 		return id[p] == id[q];
 	}
 	
+	// making connection between two objects
 	public void union(int p, int q) {
 		
 		int pid = id[p];
